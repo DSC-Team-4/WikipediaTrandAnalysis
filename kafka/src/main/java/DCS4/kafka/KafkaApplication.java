dee8ef96eb65f033f688f22c90a3027dd8dcb142
ajroot5685 <anjeong8577@naver.com>
2024-05-14 17:48:24 +0900

    test: 카프카 테스팅 코드 완성

--- kafka/src/main/java/DCS4/kafka/KafkaApplication.java
+++ kafka/src/main/java/DCS4/kafka/KafkaApplication.java
@@ -1,17 +1,12 @@
 package DCS4.kafka;
 
-import DCS4.kafka.wikiapi.WikiEventService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
-//		ConfigurableApplicationContext context = SpringApplication.run(KafkaApplication.class, args);
-//		WikiEventService service = context.getBean(WikiEventService.class);
-//		service.subscribeWikiEvents();
 	}
 }
