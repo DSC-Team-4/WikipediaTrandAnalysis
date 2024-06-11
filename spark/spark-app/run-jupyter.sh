0f09dbec95bda7d19e756241b10218ce214c5222
Philip_Cheon <51325183+popopy0412@users.noreply.github.com>
2024-06-11 18:21:28 +0900

    Delete spark directory

--- spark/spark-app/run-jupyter.sh
+++
@@ -1,2 +0,0 @@
-# ./run-jupyter.sh
-sudo docker exec -it jupyter /bin/bash
--- spark/spark-app/run-spark.sh
+++
@@ -1,5 +0,0 @@
-# ./run-spark.sh
-sudo docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
-  --master spark://spark-master:7077 \
-  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 \
-  /opt/spark-app/spark.py
